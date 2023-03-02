a96a1525229c9f960a5f1569f8ad62495f63682a
jeongseongcheol <tjdcjf9333@gmail.com>
2023-03-02 22:45:50 +0900

    캡슐화 전  css

--- CSS.js
+++ CSS.js
@@ -28,33 +28,32 @@
       root.style.alignItems = "flex";
 
       //root > main style
-      main.style.width= "500px"; 
-      main.style.height= "500px"; 
-      main.style.border= "1px dotted black"; 
+      root.children[0].style.width= "500px"; 
+      root.children[0].style.height= "500px"; 
+      root.children[0].style.border= "1px dotted black"; 
           /* flex set */ 
-      main.style.display="flex"; 
-      main.style.justifyContent= "center"; 
-      main.style.alignItems= "center"; 
+      root.children[0].style.display="flex"; 
+      root.children[0].style.justifyContent= "center"; 
+      root.children[0].style.alignItems= "center"; 
 
       //root > main > section[0]
-      for(let a= 0; a<main.children.length; a++){
-      main.children[a].style.width= "200px";
-      main.children[a].style.height="450px"; 
-        /* flex set */ 
-      main.children[a].style.display= "flex"; 
-      main.children[a].style.justifyContent=  "center"; 
-      main.children[a].style.alignItems= "center"; 
+      for(let a= 0; a<root.children[0].children.length; a++){
+      root.children[0].children[a].style.width= "200px";
+      root.children[0].children[a].style.height="450px"; 
+        /* flex set*/
+      root.children[0].children[a].style.display= "flex"; 
+      root.children[0].children[a].style.justifyContent=  "center"; 
+      root.children[0].children[a].style.alignItems= "center"; 
       }
-      sectionLeft.style.border="1px solid black";
-      sectionRight.style.border="1px solid blue";
+      root.children[0].children[0].style.border="1px solid black";
+      root.children[0].children[1].style.border="1px solid blue";
 
+      //root > main > section[0] >ul 
+      root.children[0].children[0].children[0].style.listStyleType = "none";
       //root > main > section[0] >ul >li
-      
-      ul.style.listStyleType = "none";
-      //root > main > section[0] >ul >li
-      for(j=0; j<ul.children.length; j++){
-        ul.children[j].style.width= "100%"; 
-        ul.children[j].style.height= "50px"; 
-        ul.children[j].style.backgroundColor= "#fff"; 
-        ul.children[j].style.border= "1px solid black"; 
+      for(j=0; j<root.children[0].children[0].children[0].children.length; j++){
+        root.children[0].children[0].children[0].children[j].style.width= "100%"; 
+        root.children[0].children[0].children[0].children[j].style.height= "50px"; 
+        root.children[0].children[0].children[0].children[j].style.backgroundColor= "#fff"; 
+        root.children[0].children[0].children[0].children[j].style.border= "1px solid black"; 
       }
